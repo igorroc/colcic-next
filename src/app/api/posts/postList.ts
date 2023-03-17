035f7d6130353ab6f9d69a62cf496dac85dcdd6f
Igor Rocha <igor_roc@hotmail.com.br>
2023-03-17 15:27:45 -0300

    feat: Small api to fetch data

---
+++ src/app/api/posts/postList.ts
@@ -0,0 +1,51 @@
+export interface PostType {
+	image: string
+	title: string
+	description: string
+	categories: string[]
+	body: string
+}
+
+export const postList = [
+	{
+		image: "https://images.unsplash.com/photo-1535223289827-42f1e9919769?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+		title: "Metaverso: a nova fronteira da internet",
+		description: "Nos últimos anos, o termo 'metaverso' tem sido cada vez mais utilizado para descrever um futuro em que as fronteiras entre o mundo físico e o virtual se tornam cada vez mais borradas. Mas o que exatamente é o metaverso e como ele está mudando a forma como interagimos com o mundo virtual?",
+		categories: ["Tecnologia", "Inovação"],
+		body: `
+Nos últimos anos, o termo "metaverso" tem sido cada vez mais utilizado para descrever um futuro em que as fronteiras entre o mundo físico e o virtual se tornam cada vez mais borradas. Mas o que exatamente é o metaverso e como ele está mudando a forma como interagimos com o mundo virtual?
+
+![Imagem do metaverso](https://images.unsplash.com/photo-1535223289827-42f1e9919769?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80)
+
+## O que é o metaverso?
+
+O metaverso é um termo cunhado por Neal Stephenson em seu livro "Snow Crash", publicado em 1992. Ele descreve um espaço virtual compartilhado, acessível por pessoas de todo o mundo, onde é possível interagir com outras pessoas e objetos virtuais em tempo real. 
+
+Desde então, a ideia do metaverso evoluiu para incluir não apenas jogos e mundos virtuais, mas também experiências sociais, econômicas e culturais que se estendem além dos limites do mundo físico.
+
+## Como o metaverso está mudando o mundo virtual?
+
+O metaverso está mudando fundamentalmente a forma como as pessoas interagem com o mundo virtual. Aqui estão algumas das principais maneiras como isso está acontecendo:
+
+### Realidade virtual e aumentada
+
+Com o avanço da tecnologia de realidade virtual e aumentada, as experiências virtuais estão se tornando cada vez mais imersivas. No metaverso, isso significa que as pessoas podem se sentir como se estivessem fisicamente presentes em um espaço virtual e interagindo com outras pessoas e objetos de maneira mais realista.
+
+### Economia virtual
+
+O metaverso também está criando novas oportunidades econômicas, à medida que os usuários podem comprar e vender bens e serviços virtuais em um mercado global. Isso já está acontecendo em jogos como Second Life e Minecraft, mas pode se expandir para incluir uma ampla gama de experiências virtuais no futuro.
+
+### Colaboração e criação de conteúdo
+
+O metaverso também está mudando a forma como as pessoas colaboram e criam conteúdo. Em vez de trabalhar separadamente em projetos, as pessoas podem se reunir em espaços virtuais para trabalhar juntas em tempo real. Isso pode levar a um aumento na criatividade e inovação.
+
+## O futuro do metaverso
+
+Embora o metaverso ainda esteja em seus estágios iniciais, é provável que desempenhe um papel cada vez mais importante em nossas vidas nos próximos anos. Empresas como Facebook, Google e Microsoft já estão investindo em tecnologias relacionadas ao metaverso, e há um grande potencial para a criação de novas experiências virtuais que mudam fundamentalmente a forma como nos relacionamos com o mundo virtual.
+
+Se você quiser saber mais sobre o metaverso, confira [este artigo](https://pt.wikipedia.org/wiki/Metaverso) da Wikipedia ou assista a esta [palestra](https://www.ted.com/talks/philip_rosedale_second_life_and_beyond) do fundador do Second Life, Philip Rosedale.
+        `,
+		author: "igorroc",
+		date: "2021-09-01",
+	},
+]
---
+++ src/app/api/users/userList.ts
@@ -0,0 +1,8 @@
+export const userList = [
+	{
+		name: "Igor Rocha",
+		username: "igorroc",
+		email: "igor_roc@hotmail.com.br",
+		photo: "https://github.com/igorroc.png",
+	},
+]
