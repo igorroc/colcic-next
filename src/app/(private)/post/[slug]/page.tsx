91371647a4629f76924eef87b2f788e71a4df71d
adriangvaldes <adriangvaldes@gmail.com>
2023-06-13 09:52:38 -0300

    first init

--- src/app/(private)/post/[slug]/page.tsx
+++ src/app/(private)/post/[slug]/page.tsx
@@ -1,4 +1,11 @@
-import React from "react"
+"use client"
+import { Footer } from "@/components/Footer"
+import { Header } from "@/components/Header"
+import { PostT, getPostBySlug } from "@/hooks/usePosts"
+import React, { useEffect, useState } from "react"
+
+import styles from "./page.module.css";
+import { formatToDate } from "@/utils/formatToDate"
 
 interface PostType {
 	params: {
@@ -7,9 +14,35 @@ interface PostType {
 }
 
 export default function Post({ params }: PostType) {
+	const [post, setPost] = useState<PostT>()
+	const date =
+		useEffect(() => {
+			async function loadPostInfo() {
+				const postFetched = await getPostBySlug(params.slug);
+				setPost(postFetched)
+			}
+			loadPostInfo();
+		}, [])
+
+
 	return (
 		<div>
-			<h1>Post {params.slug}</h1>
+			<Header />
+			{!!post && <div className={styles.mainContainer}>
+				<div className={styles.postHeader}>
+					<div>
+						<span className={styles.categoriesTag}>{post?.categories[0].name}</span>
+						<h1 className={styles.postHeaderTitle}>{post?.title}</h1>
+					</div>
+					<div>
+						<div >
+							<p style={{ color: 'white', fontWeight: 'bold', margin: 0, fontSize: 12 }}>{post?.author.name}</p>
+							<p style={{ margin: 0, color: 'var(--background-color-2)', fontStyle: 'italic', fontSize: 12 }}>{formatToDate(post.created_at)}</p>
+						</div>
+					</div>
+				</div>
+			</div>}
+			<Footer />
 		</div>
 	)
 }
---
+++ src/utils/formatToDate.ts
@@ -0,0 +1,9 @@
+export function formatToDate(date: string) {
+  const dateObject = new Date(date);
+  let day: any = dateObject.getDate();
+  let month: any = dateObject.getMonth();
+  if (month < 10) month = `0${month}`
+  if (day < 10) day = `0${day}`
+  let year = dateObject.getFullYear();
+  return `${day}/${month}/${year}`.replaceAll(' ', '')
+}
