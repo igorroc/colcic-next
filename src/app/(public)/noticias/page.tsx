2a03ef8c5cbd369c6b7f46ae965bddf94b100329
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-20 13:37:53 -0300

    feat: Lista de noticias para teste

--- src/app/(public)/noticias/page.tsx
+++ src/app/(public)/noticias/page.tsx
@@ -1,10 +1,29 @@
+import { TPost } from "@/types/post"
 import React from "react"
 
-export default function Noticias() {
+export default async function Noticias() {
+	const api_url = process.env.NEXT_PUBLIC_API_URL
+	const url = `${api_url}/posts`
+
+	const response = await fetch(url)
+	const posts = await response.json()
+
 	return (
 		<div>
 			<h1>Lista</h1>
-			<a href="/noticias/metaverso-post">Metaverso</a>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					gap: "1rem",
+				}}
+			>
+				{posts.map((post: TPost, index: number) => (
+					<a href={`/noticias/${post.slug}`} key={index}>
+						{post.title}
+					</a>
+				))}
+			</div>
 		</div>
 	)
 }
