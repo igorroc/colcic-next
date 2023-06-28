b38611c5cfa875aea28c972a833f89052ae17799
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-28 16:59:19 -0300

    fix: Mural timer delay

--- src/components/MuralTimer/index.tsx
+++ src/components/MuralTimer/index.tsx
@@ -1,6 +1,3 @@
-"use client"
-
-import { useEffect, useState } from "react"
 import styles from "./mural_timer.module.css"
 
 type MuralTimerProps = {
@@ -8,33 +5,11 @@ type MuralTimerProps = {
 }
 
 export default function MuralTimer(props: MuralTimerProps) {
-	const [width, setWidth] = useState(0)
-	const fps = 10
-	const passo = (fps * 100) / props.delay
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setWidth((prevWidth) => {
-				if (prevWidth >= 100) {
-					return 0
-				}
-
-				return prevWidth + passo
-			})
-		}, fps)
-
-		return () => clearTimeout(interval)
-
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
-
 	return (
 		<div className={styles.timer}>
 			<div
 				className={styles.timerBar}
-				style={{
-					width: `${width}%`,
-				}}
+				style={{ "--delay": `${props.delay}ms` } as React.CSSProperties}
 			></div>
 		</div>
 	)
