40c10794ed0a89f562de33bea5daa0ed937618d5
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-28 08:27:03 -0300

    fix: Changeable folder

---
+++ src/changeable/composicao-colegiado.tsx
@@ -0,0 +1,165 @@
+export const composicao = [
+	{
+		materia: "Álgebra",
+		departamento: "Matemática",
+		membro: "Prof. Eduardo Silva Palmeira",
+	},
+	{
+		materia: "Administração de empresa",
+		departamento: "DCAC",
+		membro: "Profa. Clemilda Gonzaga Santos",
+	},
+	{
+		materia: "Arquitetura e organização de computadores",
+		departamento: "Computação",
+		membro: "Profa. Susana Marrero Iglesiasa",
+	},
+	{
+		materia: "Banco de dados",
+		departamento: "Computação",
+		membro: "Profa. Tricia Souto Santos",
+	},
+	{
+		materia: "Cálculo diferencial e integral",
+		departamento: "Matemática",
+		membro: "Profa. Mirela Vanina de Mello",
+	},
+	{
+		materia: "Direito e legislação",
+		departamento: "DCJUR",
+		membro: "Prof. Wagner de Oliveira Rodrigues ",
+	},
+	{
+		materia: "Eletrônica",
+		departamento: "Computação",
+		membro: "Profa. Martha Ximena Torres Delgado",
+	},
+	{
+		materia: "Engenharia de software",
+		departamento: "Computação",
+		membro: "Prof. Luciano Ângelo de Souza Bernardes",
+	},
+	{
+		materia: "Economia",
+		departamento: "Economia",
+		membro: "Prof. Pedro Lopes Marinho",
+	},
+	{
+		materia: "Estágio",
+		departamento: "Computação",
+		membro: "Prof. Marcelo Ossamu Honda",
+	},
+	{
+		materia: "Física",
+		departamento: "Física",
+		membro: "Prof. Marcelo O'Donnell Krause",
+	},
+	{
+		materia: "Inglês",
+		departamento: "DLA",
+		membro: "Profa. Walkíria França Vieira Teixeira",
+	},
+	{
+		materia: "Inteligência artificial",
+		departamento: "Computação",
+		membro: "Prof. Paulo Eduardo Ambrósio",
+	},
+	{
+		materia: "Linguagens de programação",
+		departamento: "Computação",
+		membro: "Prof. Hélder Conceição Almeida",
+	},
+	{
+		materia: "Lógica matemática",
+		departamento: "Computação",
+		membro: "Prof. Francisco Bruno Souza Oliveira",
+	},
+	{
+		materia: "Metodologia da pesquisa científica",
+		departamento: "DFCH",
+		membro: "Profa. Ana Paula de Almeida Andrade",
+	},
+	{
+		materia: "Probabilidade e estatística",
+		departamento: "Estatística",
+		membro: "Prof. José Cláudio Faria",
+	},
+	{
+		materia: "Processamento de sinais",
+		departamento: "Computação",
+		membro: "Profa. Vânia Cordeiro da Silva",
+	},
+	{
+		materia: "Programação e estrutura de dados",
+		departamento: "Computação",
+		membro: "Prof. Dany Sanchez Dominguez",
+	},
+	{
+		materia: "Redes de transmissão de dados",
+		departamento: "Computação",
+		membro: "Prof. Mathias Santos de Brito",
+	},
+	{
+		materia: "Sistemas de informação e bancos de dados",
+		departamento: "Computação",
+		membro: "Prof. Sérgio Fred Ribeiro Andrade",
+	},
+	{
+		materia: "Sistemas operacionais",
+		departamento: "Computação",
+		membro: "Prof. Antonio Henrique Figueira Louro",
+	},
+	{
+		materia: "Sociologia",
+		departamento: "Computação",
+		membro: "Prof. Hamilton José Brumatto",
+	},
+	{
+		materia: "Teoria da computação",
+		departamento: "Computação",
+		membro: "Profa. César Alberto Bravo Pariente",
+	},
+	{
+		materia: "Tópicos em computação",
+		departamento: "Computação",
+		membro: "Prof. Esbel Tomas Valero Orellana",
+	},
+	{
+		materia: "Representação Discente",
+		departamento: "Computação",
+		membro: "Sra. Brenda Castro",
+	},
+	{
+		materia: "Representação Discente",
+		departamento: "Computação",
+		membro: "Sr. Brendon Souza",
+	},
+	{
+		materia: "Representação Discente",
+		departamento: "Computação",
+		membro: "Sr. Igor Vieira",
+	},
+	{
+		materia: "Representação Discente",
+		departamento: "Computação",
+		membro: "Sra. Isabelle Silva dos Santos da Cruz",
+	},
+	{
+		materia: "Representação Discente",
+		departamento: "Computação",
+		membro: "Sr. Raphael Viana",
+	},
+	{
+		materia: "Representação Discente",
+		departamento: "Computação",
+		membro: "Sra. Solana Ametista Lemos",
+	},
+]
+
+export const coordenacao = [
+	{ name: "Hélder Conceição Almeida", role: "Coordenador" },
+	{ name: "Trícia Souto Santos", role: "Vice-Coordenadora" },
+	{ name: "Rita de Cássia Souza Dias da Silva", role: "Secretária" },
+	{ name: "Ramon Santos Costa", role: "Técnico de Informática" },
+	{ name: "Flávio Victor Souza de Melo", role: "Técnico de Informática" },
+]
