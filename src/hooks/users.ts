50da5bd18113a006e637b84c9c76b07e2b263bd3
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-28 20:03:36 -0300

    feat: New user

--- src/hooks/users.ts
+++ src/hooks/users.ts
@@ -1,9 +1,23 @@
 import UserPhoto from "/public/igor.png"
+import UserPhoto2 from "/public/bia.png"
 
-export function getUser() {
-	return {
+const users = [
+	{
+		id: 1,
 		name: "Igor",
 		photo: UserPhoto,
 		isAdmin: true,
-	}
+	},
+	{
+		id: 2,
+		name: "Bia",
+		photo: UserPhoto2,
+		isAdmin: false,
+	},
+]
+
+export function getUser() {
+	const searchId = 1
+
+	return users.filter((user) => user.id === searchId)[0]
 }
