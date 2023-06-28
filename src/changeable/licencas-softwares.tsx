fd5612909ad135c3bb4f13f2c640e1ad5d2f9719
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-28 08:31:18 -0300

    fix: Changeable folder

---
+++ src/changeable/licencas-softwares.tsx
@@ -0,0 +1,59 @@
+import CX from "/public/softwares/cx.png"
+import RX from "/public/softwares/rx.png"
+import RSX from "/public/softwares/rsx.png"
+import OracleDatabase from "/public/softwares/oracle_database.png"
+import OracleJava from "/public/softwares/oracle_java.png"
+import Interbase from "/public/softwares/interbase.png"
+import Intel from "/public/softwares/intel.png"
+
+
+export const softwares = [
+	{
+		img: CX,
+		nome: "Embarcadero C++ Builder",
+		descricao:
+			"IDE para desenvolvimento no ambiente Windows, Android, IOS. Codificação, depuração, teste e design para implantação em desktops e plataformas móveis.",
+	},
+	{
+		img: CX,
+		nome: "Embarcadero Delphi Builder",
+		descricao:
+			"IDE com linguagem object pascal para desenvolvimento Windows, Android, IOS, Linux. Inclui bibliotecas para desenvolvimento rápido com banco de dados para web, desktop, mobile, aplicações híbridas.",
+	},
+	{
+		img: OracleDatabase,
+		nome: "Oracle Database, Big Data, BI",
+		descricao:
+			"Experiência prática com diversas ferramentas para gerenciamento de dados e serviços da nuvem, bancos de dados, data warehousing e data analytics. Banco de Dados Oracle, MySQL, NoSQL, Cloud Service",
+	},
+	{
+		img: OracleJava,
+		nome: "Oracle Java",
+		descricao:
+			"Ferramentas para desenvolvimento e serviço para IoT, Data mining, Machine learning, microservice",
+	},
+	{
+		img: RX,
+		nome: "Embarcadero RAD Studio",
+		descricao:
+			"IDE de propósito geral para programação de aplicação para ambientes windows, linux, android, IOS, usando C++ e object pascal. ",
+	},
+	{
+		img: RSX,
+		nome: "Embarcadero RAD Server",
+		descricao:
+			"Servidor de aplicativos para ambientes de aplicações windows, plataformas web e mobile híbridos, de aplicativos Delphi e C++Builder, em REST/JSON e IoT, semelhante ao Apache e IIS.",
+	},
+	{
+		img: Interbase,
+		nome: "Embarcadero InterBase",
+		descricao:
+			"Gerenciador para Banco de dados SQL, escalável, utilizável em ambientes Windows, Linux, macOS, iOS and Android.",
+	},
+	{
+		img: Intel,
+		nome: "Intel System Studio",
+		descricao:
+			"IDE para próposito geral com linguagens C e C++, para programação de aplicações para plataformas desktop, mobile nativa Android e híbrida, web. Com aplicação em OpenCL, IoT, processamento gráfico.",
+	},
+]
