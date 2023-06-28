5c4b2316b89d93d4cf387b0a777bf3aef5b6666c
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-28 08:28:23 -0300

    fix: Changeable folder

--- src/app/(public)/calendario/page.tsx
+++ src/app/(public)/calendario/page.tsx
@@ -3,28 +3,11 @@ import styles from "./page.module.css"
 
 import IconCalendar from "/public/icon_calendar.png"
 
+import { calendar } from "@/changeable/calendario-reunioes"
+
 export const metadata = {
 	title: "Calendário de reuniões",
 }
-
-const calendar = [
-	{
-		mes: "Janeiro",
-		diaDoMes: "28",
-		diaDaSemana: "Quinta-feira",
-	},
-	{ mes: "Fevereiro", diaDoMes: 25, diaDaSemana: "Quinta-feira" },
-	{ mes: "Março", diaDoMes: 29, diaDaSemana: "Segunda-feira" },
-	{ mes: "Abril", diaDoMes: 27, diaDaSemana: "Terça-feira" },
-	{ mes: "Maio", diaDoMes: 19, diaDaSemana: "Quarta-feira" },
-	{ mes: "Junho", diaDoMes: 30, diaDaSemana: "Quarta-feira" },
-	{ mes: "Julho", diaDoMes: 23, diaDaSemana: "Sexta-feira" },
-	{ mes: "Agosto", diaDoMes: 31, diaDaSemana: "Terça-feira" },
-	{ mes: "Setembro", diaDoMes: 20, diaDaSemana: "Segunda-feira" },
-	{ mes: "Outubro", diaDoMes: 22, diaDaSemana: "Sexta-feira" },
-	{ mes: "Novembro", diaDoMes: 18, diaDaSemana: "Quinta-feira" },
-	{ mes: "Dezembro", diaDoMes: 15, diaDaSemana: "Quarta-feira" },
-]
 
 export default function Calendario() {
 	return (
---
+++ src/changeable/calendario-reunioes.tsx
@@ -0,0 +1,18 @@
+export const calendar = [
+	{
+		mes: "Janeiro",
+		diaDoMes: "28",
+		diaDaSemana: "Quinta-feira",
+	},
+	{ mes: "Fevereiro", diaDoMes: 25, diaDaSemana: "Quinta-feira" },
+	{ mes: "Março", diaDoMes: 29, diaDaSemana: "Segunda-feira" },
+	{ mes: "Abril", diaDoMes: 27, diaDaSemana: "Terça-feira" },
+	{ mes: "Maio", diaDoMes: 19, diaDaSemana: "Quarta-feira" },
+	{ mes: "Junho", diaDoMes: 30, diaDaSemana: "Quarta-feira" },
+	{ mes: "Julho", diaDoMes: 23, diaDaSemana: "Sexta-feira" },
+	{ mes: "Agosto", diaDoMes: 31, diaDaSemana: "Terça-feira" },
+	{ mes: "Setembro", diaDoMes: 20, diaDaSemana: "Segunda-feira" },
+	{ mes: "Outubro", diaDoMes: 22, diaDaSemana: "Sexta-feira" },
+	{ mes: "Novembro", diaDoMes: 18, diaDaSemana: "Quinta-feira" },
+	{ mes: "Dezembro", diaDoMes: 15, diaDaSemana: "Quarta-feira" },
+]
