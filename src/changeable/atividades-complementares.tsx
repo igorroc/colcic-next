961095bea8832d715c3e947987c6e599151564a1
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-28 08:24:51 -0300

    fix: Changeable folder

---
+++ src/changeable/atividades-complementares.tsx
@@ -0,0 +1,64 @@
+export const atividades = [
+	{
+		type: "Participação",
+		title: "Centro Acadêmico de Ciência da Computação da UESC - <b>CACIC</b>",
+		description:
+			"Nos seguintes cargos: coordenador geral, membros da comissão de assuntos acadêmicos, membros da comissão de eventos, secretária e tesoureiro: 50 horas/ano, não podendo exceder o total de 100 horas.",
+	},
+	{
+		type: "Participação",
+		title: "Empresa Júnior de Ciência da Computação - <b>TecnoJr</b>",
+		description:
+			"Nos seguintes cargos: presidente, conselheiro, diretor ou trainee: 50 horas/ano",
+	},
+	{
+		type: "Participação",
+		title: "Semana de Informática da UESC - <b>SINFORM</b>",
+		description: "Nas atividades de apoio ao evento (alunos não-CACIC): 30 horas/evento",
+	},
+	{
+		type: "Participação",
+		title: "Semana de Informática da UESC - <b>SINFORM</b>",
+		description: "Na atividade de instrutor de Minicursos à comunidade: 30 horas/minicurso",
+	},
+	{
+		type: "Manutenção",
+		title: "não remunerada de Laboratórios da Computação",
+		description: "30 horas/semestre",
+	},
+	{
+		type: "Participação",
+		title: "Eventos científicos relacionados à Computação",
+		description: "Máximo 80 horas",
+	},
+	{
+		type: "Participação",
+		title: "Orientando em Projeto de Iniciação Cientifica",
+		description: "50 horas/projeto",
+	},
+	{
+		type: "Participação",
+		title: "Projetos de Extensão",
+		description: "50 horas/projeto",
+	},
+	{
+		type: "Atividades",
+		title: "Voluntárias ligadas a Computação",
+		description: "Máximo de 50 horas/ano",
+	},
+	{
+		type: "Atividades",
+		title: "Especiais apreciadas e aprovadas pelo Colegiado",
+		description: "Máximo de 50 horas/ano/atividade",
+	},
+	{
+		type: "Atividades",
+		title: "Monitoria na UESC",
+		description: "Máximo de 80 horas",
+	},
+	{
+		type: "Atividades",
+		title: "Estágio não curricular",
+		description: "Máximo de 80 horas",
+	},
+]
