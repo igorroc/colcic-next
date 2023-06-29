855e4f7669144290872323fb01c83cc4d9916100
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-28 22:21:01 -0300

    feat: User posts

--- src/app/(private)/(auth)/dashboard/userDashboard.tsx
+++ src/app/(private)/(auth)/dashboard/userDashboard.tsx
@@ -1,14 +1,54 @@
+import { getPostsByUser } from "@/hooks/posts"
 import { getUser } from "@/hooks/users"
+import Link from "next/link"
 import React from "react"
 
 export default function UserDashboard() {
 	const user = getUser()
+	const posts = getPostsByUser(user.id)
+	const postsEmAguardo = posts
 
 	return (
 		<div>
 			<h1>Dashboard</h1>
 			<p>Olá, {user.name}!</p>
-			<p>Atualmente você tem X publicações em aguardo</p>
+			{postsEmAguardo.length > 0 && <h2>🚀 Publicações em Aguardo</h2>}
+			{postsEmAguardo.length > 1 && (
+				<>
+					<p>Atualmente você tem {postsEmAguardo.length} publicações em aguardo</p>
+					<p>
+						Basta esperar que os administradores aprovem e sua publicação estará
+						disponível!
+					</p>
+				</>
+			)}
+			{postsEmAguardo.length == 1 && (
+				<>
+					<p>Atualmente você tem {postsEmAguardo.length} publicação em aguardo</p>
+					<p>
+						Basta esperar que os administradores aprovem e sua publicação estará
+						disponível!
+					</p>
+				</>
+			)}
+			<h2>📰 Minhas publicações</h2>
+			{posts.length > 0 ? (
+				<>
+					<p>
+						Atualmente você tem{" "}
+						{posts.length == 1 ? `1 publicação` : `${posts.length} publicações`}
+					</p>
+					<p>
+						<Link href="/posts">Ver publicações</Link>
+					</p>
+				</>
+			) : (
+				<>
+					<p>Você ainda não tem nenhuma publicação aprovada</p>
+					<p>Que tal começar agora mesmo?</p>
+					<Link href="/posts/new">Criar publicação</Link>
+				</>
+			)}
 		</div>
 	)
 }
--- src/app/(private)/(auth)/posts/page.tsx
+++ src/app/(private)/(auth)/posts/page.tsx
@@ -1,9 +1,24 @@
+import { getPostsByUser } from "@/hooks/posts"
+import { getUser } from "@/hooks/users"
 import React from "react"
 
 export default function Posts() {
+	const user = getUser()
+
+	const posts = getPostsByUser(user.id)
+
 	return (
 		<div>
-			<h1>Publicações</h1>
+			<h1>Minhas Publicações</h1>
+			{posts.length > 0 ? (
+				posts.map((post, index) => (
+					<div key={index}>
+						<span>{post.title}</span>
+					</div>
+				))
+			) : (
+				<></>
+			)}
 		</div>
 	)
 }
