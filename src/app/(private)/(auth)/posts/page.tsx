8a8cbbfc7dcf696ba5f3db205d8b2bdf01fefb96
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-29 17:36:07 -0300

    feat: User posts

--- src/app/(private)/(auth)/posts/page.tsx
+++ src/app/(private)/(auth)/posts/page.tsx
@@ -4,8 +4,13 @@ import { useUserToken } from "@/utils/handleUserToken"
 import { getPostsByUser } from "@/hooks/posts"
 import { getCurrentUser } from "@/hooks/users"
 import Link from "next/link"
-import { redirect } from "next/navigation"
 import React from "react"
+import { Button } from "@/components/Button"
+
+import styles from "./posts.module.css"
+import Image from "next/image"
+import { AiFillEdit } from "react-icons/ai"
+import { BsFillEyeFill } from "react-icons/bs"
 
 export default function Posts() {
 	const { token } = useUserToken()
@@ -18,16 +23,34 @@ export default function Posts() {
 		<div>
 			<h1>Minhas Publicações</h1>
 			{posts.length > 0 ? (
-				posts.map((post, index) => (
-					<div key={index}>
-						<span>{post.title}</span>
-					</div>
-				))
+				<div className={styles.row}>
+					{posts.map((post, index) => (
+						<div key={index} className={styles.post}>
+							<div className={styles.image}>
+								<Image src={post.banner} alt={post.title} />
+							</div>
+							<div className={styles.content}>
+								<b className={styles.title}>{post.title}</b>
+								<div className={styles.row}>
+									<Link
+										href={"/posts/" + post.id + "/edit"}
+										className={styles.button}
+									>
+										<AiFillEdit />
+									</Link>
+									<Link href={"/noticias/" + post.slug} className={styles.button}>
+										<BsFillEyeFill />
+									</Link>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
 			) : (
 				<>
 					<p>Você ainda não possui publicações aprovadas.</p>
 					<p>Que tal começar agora mesmo?</p>
-					<Link href="/posts/new">Criar publicação</Link>
+					<Button href={"/posts/new"} label="Criar publicação" type="primary" />
 				</>
 			)}
 		</div>
