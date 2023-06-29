d0e5b80c9c0e4cb57f6b2e0c774cb3d851c22f81
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-29 17:11:16 -0300

    feat: Admin dashboard

--- src/app/(private)/(auth)/dashboard/adminDashboard.tsx
+++ src/app/(private)/(auth)/dashboard/adminDashboard.tsx
@@ -1,9 +1,13 @@
-import { getPostsWaitingForApproval } from "@/hooks/posts"
-import { getCurrentUser } from "@/hooks/users"
-import { useUserToken } from "@/utils/handleUserToken"
-import Link from "next/link"
-import { redirect } from "next/navigation"
 import React from "react"
+import Link from "next/link"
+
+import { getPostsWaitingForApproval } from "@/hooks/posts"
+import { getCurrentUser, getTopPublishers } from "@/hooks/users"
+import { useUserToken } from "@/utils/handleUserToken"
+
+import styles from "./dashboard.module.css"
+import { Button } from "@/components/Button"
+import Image from "next/image"
 
 export default function AdminDashboard() {
 	const { token } = useUserToken()
@@ -11,19 +15,41 @@ export default function AdminDashboard() {
 	const user = getCurrentUser(token)
 	const postsWaitingForApproval = getPostsWaitingForApproval()
 
+	const topPublishers = getTopPublishers()
+
 	return (
 		<div>
 			<h1>Dashboard de Admin</h1>
 			<p>Olá, {user.name}!</p>
-			{postsWaitingForApproval.length > 0 && (
-				<>
-					<p>
-						Atualmente existem {postsWaitingForApproval.length} publicações esperando
-						aprovação.
-					</p>
-					<Link href={"/posts/approve"}>Aprovar publicações</Link>
-				</>
+			<h2>Publicações</h2>
+			{postsWaitingForApproval.length > 0 ? (
+				<div className={styles.card}>
+					<b>{postsWaitingForApproval.length}</b>
+					<p>Aguardando aprovação</p>
+					<Button href={"/posts/approve"} label="Verificar" type="secondary" />
+				</div>
+			) : (
+				<div className={styles.card}>
+					<p>Nenhuma publicação aguardando aprovação</p>
+				</div>
 			)}
+			<h2>Top publicadores</h2>
+			<div className={styles.row}>
+				{topPublishers.map((publisher) => (
+					<div className={styles.card} key={publisher.id}>
+						<div className={styles.userPhoto}>
+							<Image src={publisher.photo} alt={publisher.name} />
+						</div>
+						<span className={styles.userName}>{publisher.name}</span>
+						<span>
+							{publisher.posts > 1
+								? `${publisher.posts} posts`
+								: `${publisher.posts} post`}
+						</span>
+						{/* <Link href={`/users/${publisher.id}`}>Ver perfil</Link> */}
+					</div>
+				))}
+			</div>
 		</div>
 	)
 }
--- src/hooks/users.ts
+++ src/hooks/users.ts
@@ -1,4 +1,5 @@
 import { users } from "./userList"
+import { postList } from "./postList"
 
 export function getCurrentUser(userToken: string) {
 	return users.filter((user) => user.token === userToken)[0]
@@ -16,4 +17,23 @@ export function handleUserLogin(username: string, password: string) {
 	}
 
 	return false
+}
+
+export function getTopPublishers() {
+	// busca 5 usuários com mais posts e retorna um array com os usuários (nome, photo e quantidade), ordenados por quantidade de posts, do maior para o menor
+
+	const topPublishers = users
+		.map((user) => {
+			const posts = postList.filter((post) => post.author_id === user.id)
+
+			return {
+				id: user.id,
+				name: user.name,
+				photo: user.photo,
+				posts: posts.length,
+			}
+		})
+		.sort((a, b) => b.posts - a.posts)
+
+	return topPublishers.slice(0, 5)
 }
