42e4e47a448393537bf187567c6f97844600ffdd
Igor Rocha <igor_roc@hotmail.com.br>
2023-07-08 16:17:11 -0300

    feat: Starting new post

--- src/app/(private)/(auth)/posts/new/page.tsx
+++ src/app/(private)/(auth)/posts/new/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import {
 	Checkbox,
 	FormControl,
@@ -17,14 +17,16 @@ import {
 
 import styles from "./page.module.css"
 
-import { Button } from "@/components/Button"
-import defaultBody from "@/utils/defaultPostBody"
-
 const publishTypes = ["Site", "Mural"]
 
 export default function Editor() {
+	const [title, setTitle] = useState("")
+	const [slug, setSlug] = useState("")
+	const [hasEditedSlug, setHasEditedSlug] = useState(false)
 	const [description, setDescription] = useState("")
 	const [publiType, setPubliType] = useState<string[]>([])
+	const [bannerH, setBannerH] = useState("")
+	const [bannerV, setBannerV] = useState("")
 	const maxDescriptionLength = 100
 
 	const handleChange = (event: SelectChangeEvent<typeof publiType>) => {
@@ -37,12 +39,50 @@ export default function Editor() {
 		)
 	}
 
+	useEffect(() => {
+		if (title.length === 0) {
+			setSlug("")
+			setHasEditedSlug(false)
+			return
+		}
+		if (hasEditedSlug) return
+		setSlug(title.toLowerCase().replace(/ /g, "-"))
+	}, [title, hasEditedSlug])
+
 	return (
 		<div>
 			<h1>Criar publicação</h1>
 			<form action="" id={styles.form}>
-				<TextField label="Título" />
-				<TextField label="Banner" />
+				<TextField
+					label="Título"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<FormControl variant="outlined">
+					<InputLabel htmlFor="component-helper">Slug</InputLabel>
+					<OutlinedInput
+						id="component-helper"
+						value={slug}
+						label="Slug"
+						onChange={(e) => {
+							setHasEditedSlug(true)
+							setSlug(e.target.value)
+						}}
+						multiline
+					/>
+					<FormHelperText id="component-helper-text">
+						Slug é o nome que aparece na URL da publicação
+					</FormHelperText>
+				</FormControl>
+				<div>
+					<TextField
+						label="Banner"
+						value={bannerH}
+						onChange={(e) => setBannerH(e.target.value)}
+					/>
+					{/* eslint-disable-next-line  */}
+					{bannerH && <img src={bannerH} alt="Banner" width={100} height={100} />}
+				</div>
 				<FormControl error={description.length > maxDescriptionLength} variant="outlined">
 					<InputLabel htmlFor="component-helper">Descrição</InputLabel>
 					<OutlinedInput
@@ -56,7 +96,7 @@ export default function Editor() {
 						Caracteres restantes: {maxDescriptionLength - description.length}
 					</FormHelperText>
 				</FormControl>
-				<FormControl >
+				<FormControl>
 					<InputLabel id="demo-multiple-checkbox-label">Tipo de Postagem</InputLabel>
 					<Select
 						labelId="demo-multiple-checkbox-label"
