2cefb4f15d1b036ef1dee53a9b5cc37832fd9d0e
Igor Rocha <igor_roc@hotmail.com.br>
2023-07-09 01:09:21 -0300

    fix: Hook call with token

--- src/app/(private)/(auth)/users/[id]/delete/page.tsx
+++ src/app/(private)/(auth)/users/[id]/delete/page.tsx
@@ -24,7 +24,7 @@ export default function DeleteUser({ params }: DeleteUserProps) {
 
 	useEffect(() => {
 		async function getData() {
-			const user = await getUserById(params.id)
+			const user = await getUserById(params.id, token)
 			if (user) {
 				setFullName(user.name)
 			} else {
