2db77abfd4e9304c87c3dbbefc922e0750d39eaf
Igor Rocha <igor_roc@hotmail.com.br>
2023-07-08 21:22:23 -0300

    fix: Remove Token

--- src/app/(private)/(auth)/users/[id]/delete/page.tsx
+++ src/app/(private)/(auth)/users/[id]/delete/page.tsx
@@ -24,7 +24,7 @@ export default function DeleteUser({ params }: DeleteUserProps) {
 
 	useEffect(() => {
 		async function getData() {
-			const user = await getUserById(params.id, token)
+			const user = await getUserById(params.id)
 			if (user) {
 				setFullName(user.name)
 			} else {
