c2ea2bf459526ee515496d197162d5415569c2af
Igor Rocha <igor_roc@hotmail.com.br>
2023-07-10 07:08:07 -0300

    fix: Style and better messages

--- src/app/(private)/(auth)/dashboard/userDashboard.tsx
+++ src/app/(private)/(auth)/dashboard/userDashboard.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Button } from "@/components/Button"
 import usePosts from "@/hooks/posts"
 import useUser from "@/hooks/users"
 import { useUserToken } from "@/utils/handleUserToken"
@@ -20,19 +21,16 @@ export default function UserDashboard() {
 		<div>
 			<h1>Dashboard</h1>
 			<p>Olá, {user.name}!</p>
-			{postsEmAguardo.length > 0 && <h2>⏰ Publicações em Aguardo</h2>}
-			{postsEmAguardo.length > 1 && (
+			{postsEmAguardo.length > 0 && (
 				<>
-					<p>Atualmente você tem {postsEmAguardo.length} publicações em aguardo</p>
+					<h2>⏰ Publicações em Aguardo</h2>
 					<p>
-						Basta esperar que os administradores aprovem e sua publicação estará
-						disponível!
+						Atualmente você tem{" "}
+						{postsEmAguardo.length > 1
+							? `${postsEmAguardo.length} publicações`
+							: "1 publicação"}{" "}
+						em aguardo
 					</p>
-				</>
-			)}
-			{postsEmAguardo.length == 1 && (
-				<>
-					<p>Atualmente você tem {postsEmAguardo.length} publicação em aguardo</p>
 					<p>
 						Basta esperar que os administradores aprovem e sua publicação estará
 						disponível!
@@ -47,14 +45,18 @@ export default function UserDashboard() {
 						{posts.length == 1 ? `1 publicação!` : `${posts.length} publicações!`}
 					</p>
 					<p>
-						<Link href="/posts">Ver publicações</Link>
+						<Button
+							label={posts.length == 1 ? `Ver publicação` : `Ver publicações`}
+							type="primary"
+							href="/posts"
+						/>
 					</p>
 				</>
 			) : (
 				<>
 					<p>Você ainda não tem nenhuma publicação aprovada</p>
 					<p>Que tal começar agora mesmo?</p>
-					<Link href="/posts/new">Criar publicação</Link>
+					<Button label="Criar publicação" type="primary" href="/posts/new" />
 				</>
 			)}
 		</div>
