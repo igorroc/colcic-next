e48cf576c359f1879165e7ef087b1ca0d8a76584
Igor Rocha <igor_roc@hotmail.com.br>
2023-09-27 20:48:04 -0300

    fix: Wrong month

--- src/utils/formatToDate.ts
+++ src/utils/formatToDate.ts
@@ -1,7 +1,7 @@
 export function formatToDate(date: string) {
 	const dateObject = new Date(date)
 	let day: any = dateObject.getDate()
-	let month: any = dateObject.getMonth()
+	let month: any = dateObject.getMonth() + 1
 	if (month < 10) month = `0${month}`
 	if (day < 10) day = `0${day}`
 	let year = dateObject.getFullYear()
